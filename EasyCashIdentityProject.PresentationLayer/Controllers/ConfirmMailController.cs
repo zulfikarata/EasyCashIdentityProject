af877b366971fd481256fc4a11134801f9cc1d25
Zülfikar Ata <zulfikarata07@gmail.com>
2023-09-07 15:17:15 +0300

    Login işlemleri yapıldı

--- EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -5,36 +5,33 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
-	public class ConfirmMailController : Controller
-	{
-		private readonly UserManager<AppUser> _userManager;
-
+    public class ConfirmMailController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
         public ConfirmMailController(UserManager<AppUser> userManager)
         {
             _userManager = userManager;
         }
-
         [HttpGet]
-		public IActionResult Index()
-		{
-			var value = TempData["Mail"];
+        public IActionResult Index()
+        {
+            var value = TempData["Mail"];
             ViewBag.v = value;
-            //confirmMailViewModel.Mail = value.ToString();
-			return View();
-		}
+            //  confirmMailViewModel.Mail = value.ToString();
+            return View();
+        }
 
         [HttpPost]
         public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
         {
             var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
-			if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+            if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
             {
                 user.EmailConfirmed = true;
                 await _userManager.UpdateAsync(user);
-                return RedirectToAction("Index","MyProfile");
+                return RedirectToAction("Index", "Login");
             }
             return View();
-            
         }
     }
 }
