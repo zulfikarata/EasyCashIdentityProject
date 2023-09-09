ce5c6ee673e9f37fe0cf09844d63df98553946de
Zülfikar Ata <zulfikarata07@gmail.com>
2023-09-09 22:10:03 +0300

    Login Authorize işlemleri

--- EasyCashIdentityProject.PresentationLayer/Models/ConfirmMailViewModel.cs
+++ EasyCashIdentityProject.PresentationLayer/Models/ConfirmMailViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class ConfirmMailViewModel
     {
-        public string Mail { get; set; }
+        public string? Mail { get; set; }
         public int ConfirmCode { get; set; }
     }
 }
