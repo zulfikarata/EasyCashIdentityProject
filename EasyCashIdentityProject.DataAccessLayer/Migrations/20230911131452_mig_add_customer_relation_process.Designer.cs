df7710342e4505ee6fecaf5f59b0157622e813d9
Zülfikar Ata <zulfikarata07@gmail.com>
2023-09-14 14:55:50 +0300

    Para Transferi İşlemleri Yapıldı.

--- EasyCashIdentityProject.DataAccessLayer/Migrations/20230911131452_mig_add_customer_relation_process.Designer.cs
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230911131452_mig_add_customer_relation_process.Designer.cs
@@ -4,6 +4,7 @@ using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyCashIdentityProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230911131452_mig_add_customer_relation_process")]
+    partial class mig_add_customer_relation_process
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -186,10 +188,6 @@ namespace EasyCashIdentityProject.DataAccessLayer.Migrations
 
                     b.Property<decimal>("Amount")
                         .HasColumnType("decimal(18,2)");
-
-                    b.Property<string>("Description")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("ProcessDate")
                         .HasColumnType("datetime2");
--- EasyCashIdentityProject.DtoLayer/Dtos/CustomerAccountProcessDtos/SendMoneyForCustomerAccountProcessDto.cs
+++ EasyCashIdentityProject.DtoLayer/Dtos/CustomerAccountProcessDtos/SendMoneyForCustomerAccountProcessDto.cs
@@ -4,18 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EasyCashIdentityProject.EntityLayer.Concrete
+namespace EasyCashIdentityProject.DtoLayer.Dtos.CustomerAccountProcessDtos
 {
-    public class CustomerAccountProcess
+    public class SendMoneyForCustomerAccountProcessDto
     {
-        public int CustomerAccountProcessID { get; set; }
         public string ProcessType { get; set; }
         public decimal Amount { get; set; }
         public DateTime ProcessDate { get; set; }
         public int? SenderID { get; set; }
-        public int? ReceiverID  { get; set; }
-        public CustomerAccount SenderCustomer { get; set; }
-        public CustomerAccount ReceiverCustomer { get; set; }
+        public string ReceiverAccountNumber { get; set; }
+        public int? ReceiverID { get; set; }
         public string Description { get; set; }
     }
 }
