f0b261e5ad4daa839833f44af56d35782cd6cc54
Zülfikar Ata <zulfikarata07@gmail.com>
2023-09-19 13:07:02 +0300

    RapidAPI ile Döviz Kurları Sayfası

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ExchangeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            #region
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=TRY&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "8ef9c5a0eemsh88e2a366fbc2f14p1fc3f3jsn73cd0d3ac521" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ViewBag.UsdToTry = body;
+            }
+            #endregion
+
+            #region
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=EUR&to=TRY&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "8ef9c5a0eemsh88e2a366fbc2f14p1fc3f3jsn73cd0d3ac521" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                ViewBag.EurToTry = body2;
+            }
+            #endregion
+            return View();
+        }
+    }
+}
